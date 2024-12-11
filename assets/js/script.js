1f7cc1fbf6dfbd2c687fd584f4e0c42ab5417cb0
Rohit Bharti <71224966+Smarty6452@users.noreply.github.com>
2024-12-11 00:06:41 -0500

    optimize code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,33 +1,25 @@
-// Get modal elements
 const settingsButton = document.getElementById('settings-button');
 const settingsModal = document.getElementById('settings-modal');
 const closeModal = document.getElementById('close-modal');
 const colorButtons = document.querySelectorAll('.color-button');
 
 // Open modal
-settingsButton.addEventListener('click', () => {
-    settingsModal.style.display = 'block';
-});
+settingsButton.addEventListener('click', () => settingsModal.style.display = 'block');
 
 // Close modal
-closeModal.addEventListener('click', () => {
-    settingsModal.style.display = 'none';
-});
+closeModal.addEventListener('click', () => settingsModal.style.display = 'none');
 
 // Change background color
 colorButtons.forEach(button => {
     button.addEventListener('click', () => {
-        const selectedColor = button.getAttribute('data-color');
-        document.body.style.backgroundColor = selectedColor;
+        document.body.style.backgroundColor = button.getAttribute('data-color');
         settingsModal.style.display = 'none';
     });
 });
 
 // Close modal when clicking outside content
 window.addEventListener('click', (e) => {
-    if (e.target === settingsModal) {
-        settingsModal.style.display = 'none';
-    }
+    if (e.target === settingsModal) settingsModal.style.display = 'none';
 });
 
 $(document).ready(function () {
@@ -41,12 +33,8 @@ $(document).ready(function () {
         sliderContainer.css("transform", `translateX(-${currentIndex * 100}%)`);
     }
 
-    setInterval(autoSlide, 3000); 
+    setInterval(autoSlide, 3000);
 });
-
-// document.getElementById('hamburger-menu').addEventListener('click', function() {
-//     document.querySelector('.nav-links').classList.toggle('active');
-// });
 
 document.getElementById('hamburger-menu').addEventListener('click', function(event) {
     event.stopPropagation();
@@ -54,10 +42,8 @@ document.getElementById('hamburger-menu').addEventListener('click', function(eve
 });
 
 document.addEventListener('click', function(event) {
-    const hamburgerMenu = document.getElementById('hamburger-menu');
     const navLinks = document.querySelector('.nav-links');
-    
-    if (!hamburgerMenu.contains(event.target) && !navLinks.contains(event.target)) {
+    if (!navLinks.contains(event.target) && !event.target.closest('#hamburger-menu')) {
         navLinks.classList.remove('active');
     }
 });
